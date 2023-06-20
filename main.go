c3900bf37cb25a42e1a4ca27c23d90059c938c7c
Logan <ninetailsoftware@gmail.com>
2023-06-20 03:51:11 -0400

    reconstruction

--- main.go
+++ main.go
@@ -1,81 +1,17 @@
 package main
 
 import (
-	"errors"
-
-	"net/http"
+	"go-api/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
-type dummy struct {
-	ID         string `json:"id"`
-	Restaurant string `json:"restaurant"`
-	Good       bool   `json:"good"`
-}
-
-var dummys = []dummy{
-	{ID: "1", Restaurant: "Taco Bell", Good: false},
-	{ID: "2", Restaurant: "Wendy's", Good: false},
-	{ID: "3", Restaurant: "Mcdonald's", Good: false},
-	{ID: "4", Restaurant: "Arby's", Good: false},
-}
-
-func getDummys(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, dummys)
-}
-
-func addDummy(context *gin.Context) {
-	var newDummy dummy
-
-	if err := context.BindJSON(&newDummy); err != nil {
-		return
-	}
-
-	dummys = append(dummys, newDummy)
-	context.IndentedJSON(http.StatusCreated, newDummy)
-}
-
-func getDummyById(id string) (*dummy, error) {
-	for i, d := range dummys {
-		if d.ID == id {
-			return &dummys[i], nil
-		}
-	}
-
-	return nil, errors.New("Dummy Not Found")
-}
-
-func getDummy(context *gin.Context) {
-	id := context.Param("id")
-	dummy, err := getDummyById(id)
-
-	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Dummy Not Found."})
-		return
-	}
-
-	context.IndentedJSON(http.StatusOK, dummy)
-}
-
-func modifyDummy(context *gin.Context) {
-	id := context.Param("id")
-	dummy, err := getDummyById(id)
-
-	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Dummy Not Found."})
-		return
-	}
-
-	dummy.Good = !dummy.Good
-	context.IndentedJSON(http.StatusOK, dummy)
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/dummys", getDummys)
-	router.GET("/dummys/:id", getDummy)
-	router.PATCH("/dummys/:id", modifyDummy)
-	router.POST("/dummys", addDummy)
+
+	router.GET("/Jobs", controllers.GetJobs)
+	router.GET("/Jobs/:id", controllers.GetJob)
+	router.PATCH("/Jobs/:id", controllers.ModifyJob)
+	router.POST("/Jobs", controllers.AddJob)
 	router.Run("localhost:9090")
 }
